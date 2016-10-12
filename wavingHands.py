fb0d45e1afa1be7ddc59be52264f6dc31f329484
Szeghi Dorottya <szeghi.d@gmail.com>
2016-10-12 11:00:44 +0200

    masodik,func, kör, és dict

--- wavingHands.py
+++ wavingHands.py
@@ -2,17 +2,9 @@ fLeft=""
 fRight=""
 sLeft=""
 sRight=""
-fDamage=0
-sDamage=0
-
-spells={ "WPP" : "Counter-spell",
-         "WWS": "Counter-spell",
-         "P": "Shield", "SD": "Missile", 
-         "DFFDD": "Lightning Bolt", 
-         "WDDC": "Lightning Bolt",
-         "WFP": "Cause Light Wounds", 
-         "WPFD": "Cause Heavy Wounds",
-         ">": "Stab"}
+#damage taken
+fDmg=0
+sDmg=0
 
 
 
@@ -22,11 +14,13 @@ def input_check(letter):
     return letter
 
 def spells_check(hand):
-    for each in spells:
-        if hand == spells[each]:
-            print("jó")
-        else:
-            print("nem jó")
+    for each in gestures:
+        if hand == gestures[each]:
+            #print("jó")
+            func=spells[hand]
+            func()
+        #else:
+            #print("nem jó")
 
 
 def hand_reading(fLeft,fRight,sLeft,sRight):
@@ -42,7 +36,59 @@ def hand_reading(fLeft,fRight,sLeft,sRight):
     secondR=input("2. játékos jobbkéz: ")
     sRight=sRight+input_check(secondR)
 
+def game_over(fDmg):
+    if fDmg==15:
+        print("\nJáték vége!\nA második játékos nyert!\n")
+    else:
+        print("\nJáték vége!\nAz első játékos nyert!\n")
 
-hand_reading(fLeft,fRight,sLeft,sRight)
-#spells_check(fLeft)
+def rounds(fLeft,fRight,sLeft,sRight,fDmg,sDmg):
+    i=1
+    if fDmg!=15 and sDmg!=15:
+        print("\n",i,". kör\n")
+        hand_reading(fLeft,fRight,sLeft,sRight)
+        fDmg=fDmg+3
+        print(fDmg)
+        rounds(fLeft,fRight,sLeft,sRight,fDmg,sDmg)
+    else:
+        game_over(fDmg)
+
+
+def counter_spell():
+    print("Counter-spell")
+
+def shield():
+    print("Shield")
+
+def missile():
+    print("Missile")
+
+def lightning_bolt():
+    print("Lightning Bolt")
+
+def cause_light_wounds():
+    print("Cause Light Wounds")
+
+def cause_heavy_wounds():
+    print("Cause Heavy Wounds")
+
+
+gestures= {0:"WPP",1:"WWS",2:"P",3:"SD",4:"DFFDD",5:"WDDC",6:"WFP",7:"WPFD",8:">"}
+
+spells={ "WPP" : counter_spell,
+         "WWS": counter_spell,
+         "P": shield, 
+         "SD": missile, 
+         "DFFDD": lightning_bolt, 
+         "WDDC": lightning_bolt,
+         "WFP": cause_light_wounds, 
+         "WPFD": cause_heavy_wounds,
+         ">": "Stab"}
+
+rounds(fLeft,fRight,sLeft,sRight,fDmg,sDmg)
+spells_check("SD")
+#lines=["PL","WPFD","SD","WPP"]
+#for line in lines:
+#    func=spells[line]
+#    func()
 

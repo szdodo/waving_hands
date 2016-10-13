4b5259b31cf24a387f04d5a09bf20d040654a6fb
Szeghi Dorottya <szeghi.d@gmail.com>
2016-10-13 10:19:14 +0200

    6. dmg és protection műkszik

--- wavingHands.py
+++ wavingHands.py
@@ -5,6 +5,8 @@ sRight=""
 #damage taken
 fDmg=0
 sDmg=0
+fProtection=False
+sProtection=False
 
 def input_check(letter):
     while letter!='W' and letter!='F' and letter!='P' and letter!='S' and letter!='D' and letter!='>' and letter !='':
@@ -23,7 +25,7 @@ def spell_check(hand,num):
         
 
 #firtsL.. - beolvasáshoz kell, fLeft amit használunk
-def hand_reading(fL,fR,sL,sR): 
+def hand_reading(): 
     global fLeft
     firstL=input("1. játékos bal kéz: ")
     fLeft=str(fLeft)+str(input_check(firstL))
@@ -49,60 +51,111 @@ def hand_reading(fL,fR,sL,sR):
     sRight=spell_check(sRight,2)
     print(sRight)
 
-def game_over(fDmg):
+def game_over():
     if fDmg==15:
         print("\nJáték vége!\nA második játékos nyert!\n")
     else:
         print("\nJáték vége!\nAz első játékos nyert!\n")
 
-def rounds(fL,fR,sL,sR,fDmg, sDmg):
-    if fDmg!=15 and sDmg!=15:
+def rounds(fL,fR,sL,sR):
+    global fDmg
+    global sDmg
+    global fProtection
+    global sProtection
+    if fDmg<15 and sDmg<15:
         print("\n",1,". kör\n")
-        hand_reading(fL,fR,sL,sR)
-        fDmg+=3
+        fProtection=False
+        sProtection=False
+        hand_reading()
+        fDmg+=2
+        print(sDmg)
         print(fDmg)
-        rounds(fL,fR,sL,sR,fDmg,sDmg)
+        print(fProtection,sProtection)
+        rounds(fL,fR,sL,sR)
     else:
-        game_over(fDmg)
+        game_over()
 
 
 def counter_spell(num):
+    global fProtection
+    global sProtection
+    
+    if num==1:
+        fProtection=True
+        print(fProtection)
+    else:
+        sProtection=True
+        print(sProtection)
     print("Counter-spell")
 
 def shield(num):
+    global fProtection
+    global sProtection
+
+    if num==1:
+        fProtection=True
+        print(fProtection)
+    else:
+        sProtection=True
+        print(sProtection)
     print("Shield")
 
 def missile(num):
+    global fDmg
+    global sDmg
+    if num==1 and sProtection==False:
+        sDmg=sDmg+1
+    if num==2 and fProtection==False:
+        fDmg=fDmg+1
+    
     print("Missile")
 
 def lightning_bolt(num):
+    global fDmg
+    global sDmg
+    if num==1 and sProtection==False:
+        sDmg=sDmg+5
+    if num==2 and fProtection==False:
+        fDmg=fDmg+5
     print("Lightning Bolt")
 
 def cause_light_wounds(num):
+    global fDmg
+    global sDmg
+    if num==1 and sProtection==False:
+        sDmg=sDmg+2
+    if num==2 and fProtection==False:
+        fDmg=fDmg+2
     print("Cause Light Wounds")
 
 def cause_heavy_wounds(num):
+    global fDmg
+    global sDmg
+    if num==1 and sProtection==False:
+        sDmg=sDmg+3
+    if num==2 and fProtection==False:
+        fDmg=fDmg+3
     print("Cause Heavy Wounds")
 
 
-gestures= {0:"WPP",1:"WWS",2:"P",3:"SD",4:"DFFDD",5:"WDDC",6:"WFP",7:"WPFD",8:">"}
+gestures= {0:"WPP",1:"WWS",2:"PS",3:"SD",4:"DFFDD",5:"WDD",6:"WFP",7:"WPFD",8:">"}
 
 #WDDC-lightning bolt, nincs clap mert ahhoz 2 kéz kell
 spells={ "WPP" : counter_spell,
          "WWS": counter_spell,
-         "P": shield, 
          "SD": missile, 
          "DFFDD": lightning_bolt, 
          "WDD": lightning_bolt,
          "WFP": cause_light_wounds, 
          "WPFD": cause_heavy_wounds,
+         "PS": shield,
          ">": "Stab"}
 
-rounds(fLeft,fRight,sLeft,sRight,fDmg, sDmg)
-spell=spell_check("WPFSSDWPP")
+rounds(fLeft,fRight,sLeft,sRight)
+#spell=spell_check("WPFSSDWPP")
 #lines=["PL","WPFD","SD","WPP"]
 #for line in lines:
 #    func=spells[line]
 #    func()
-alma="WSSWWS"
+#alma="WSSWWS"
 #print(alma.find("WSW"))
